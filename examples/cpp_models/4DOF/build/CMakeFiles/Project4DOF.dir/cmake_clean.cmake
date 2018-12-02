c373ebb78b22fa2b66f4694d7d9206e1beef01e3
Marcus <hoergems@gmail.com>
2018-12-02 20:30:09 +1000

    First commit

---
+++ examples/cpp_models/4DOF/build/CMakeFiles/Project4DOF.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project4DOF.dir/src/base/base_rock_sample.cpp.o"
+  "CMakeFiles/Project4DOF.dir/src/rock_sample/rock_sample.cpp.o"
+  "CMakeFiles/Project4DOF.dir/src/rock_sample/main.cpp.o"
+  "Project4DOF.pdb"
+  "Project4DOF"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project4DOF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ examples/cpp_models/4DOF/build/CMakeFiles/Project_fvrs.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_fvrs.dir/src/base/base_rock_sample.cpp.o"
+  "CMakeFiles/Project_fvrs.dir/src/fvrs/fvrs.cpp.o"
+  "CMakeFiles/Project_fvrs.dir/src/fvrs/main.cpp.o"
+  "Project_fvrs.pdb"
+  "Project_fvrs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_fvrs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
